1aeb513916ce9706eab0778d67d46a4d390ddb0f
ConorDavenport <cnrdavenport@gmail.com>
2024-03-15 16:12:55 +0000

    main: get led to turn on

--- examples/blinky/src/main.c
+++ examples/blinky/src/main.c
@@ -1,6 +1,18 @@
+#include <stdint.h>
 #include "stm32f7xx_hal.h"
+#include "stm32f7xx_hal_gpio.h"
+#include "stm32f722xx.h"
 
 int main() {
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    GPIO_InitTypeDef g;
+    g.Mode = GPIO_MODE_OUTPUT_PP;
+    g.Pull = GPIO_NOPULL;
+    g.Speed = GPIO_SPEED_LOW;
+    g.Pin = GPIO_PIN_7;
+    HAL_GPIO_Init(GPIOB, &g);
+    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
+
     while(1) {
 
     }
