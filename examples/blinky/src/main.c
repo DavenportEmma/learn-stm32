282ec0bdc93e700c39084c2e0b712573e1e65e9e
Conor Davenport <conor.davenport@microchip.com>
2024-03-26 15:38:22 +0000

    :)

--- examples/blinky/src/main.c
+++ examples/blinky/src/main.c
@@ -1,10 +1,25 @@
 #include <stdint.h>
 #include "stm32f7xx_hal.h"
 #include "stm32f7xx_hal_gpio.h"
+#include "stm32f7xx_hal_tim.h"
 #include "stm32f722xx.h"
+
+TIM_HandleTypeDef htim;
 
 int main() {
     HAL_Init();
+
+    htim.Instance = TIM2;
+    htim.Channel = HAL_TIM_ACTIVE_CHANNEL_1;
+    htim.Init.Prescaler = 999;
+    htim.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim.Init.Period = 7199;
+    htim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim.Init.RepetitionCounter = 0;
+    htim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+
+    HAL_TIM_Base_Init(&htim);
+
     __HAL_RCC_GPIOB_CLK_ENABLE();
     GPIO_InitTypeDef g;
     g.Mode = GPIO_MODE_OUTPUT_PP;
@@ -14,10 +29,13 @@ int main() {
     HAL_GPIO_Init(GPIOB, &g);
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
 
-    while(1) {
-        HAL_Delay(1000);
-        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
-    }
+    HAL_TIM_Base_Start_IT(&htim);
+
+    while(1) {}
 
     return 0;
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim) {
+    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_7);
 }
