7611966ca1c749bb6b8559c04728e483a4e28689
Emma Davenport <emma.v.davenport@gmail.com>
2024-08-05 20:44:26 +0100

    tasks: handle multiple concurrent sequences

--- examples/sequencer/src/tasks.c
+++ examples/sequencer/src/tasks.c
@@ -1,43 +1,69 @@
 #include "FreeRTOSConfig.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 #include "tasks.h"
 #include "uart.h"
 #include "start_up.h"
-
+#include "common.h"
 #include "sequence.h"
 
-void MIDI_task() {
-    uint8_t stack_counter = 0;
-
+void MIDI_task(void* pvParameters) {
+    MIDISequence_TypeDef* sequence = (MIDISequence_TypeDef*) pvParameters;
+    
     while(1) {
-        MIDIPacket_TypeDef previous_step;
-        if(stack_counter == 0) {
-            previous_step = midi_sequence[SEQUENCE_LENGTH - 1];
+        TickType_t lastWakeTime = xTaskGetTickCount();
+
+        MIDIStep_TypeDef previous_step;
+        MIDIStep_TypeDef this_step = sequence->steps[sequence->counter];
+
+        if(sequence->counter == 0) {
+            previous_step = sequence->steps[sequence->num_steps - 1];
         } else {
-            previous_step = midi_sequence[stack_counter - 1];
+            previous_step = sequence->steps[sequence->counter - 1];
         }
 
-        if(previous_step.glide == 0) {
-            previous_step.status = NOTE_OFF;
-            send_midi_note(USART1, &previous_step);
+        MIDIPacket_TypeDef previous_packet = {
+            previous_step.status,
+            sequence->channel,
+            previous_step.note,
+            previous_step.velocity,
+            previous_step.glide
+        };
+
+        MIDIPacket_TypeDef this_packet = {
+            this_step.status,
+            sequence->channel,
+            this_step.note,
+            this_step.velocity,
+            this_step.glide
+        };
+
+        if(xSemaphoreTake(MIDI_mutex, portMAX_DELAY) == pdTRUE) {
+            if(previous_step.glide == 0) {
+                previous_packet.status = NOTE_OFF;
+                send_midi_note(USART1, &previous_packet);
+            }
+
+            if(send_midi_note(USART1, &this_packet) > 0) {
+                char msg[] = "timeout\n\r";
+                send(USART3, msg, sizeof(msg)-1);
+            }
+
+            if(previous_step.glide == 1) {
+                previous_packet.status = NOTE_OFF;
+                send_midi_note(USART1, &previous_packet);
+            }
+
+            xSemaphoreGive(MIDI_mutex);
         }
 
-        if(send_midi_note(USART1, &midi_sequence[stack_counter]) > 0) {
-            char msg[] = "timeout\n\r";
-            send(USART3, msg, sizeof(msg)-1);
+        if(this_step.end_of_sequence) {
+            sequence->counter = 0;
+        } else {
+            sequence->counter++;
         }
 
-        if(previous_step.glide == 1) {
-            previous_step.status = NOTE_OFF;
-            send_midi_note(USART1, &previous_step);
-        }
-
-        stack_counter++;
-        if(stack_counter >= SEQUENCE_LENGTH) {
-            stack_counter = 0;
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(200));
+        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(200) * sequence->divisor);
     }
 }
