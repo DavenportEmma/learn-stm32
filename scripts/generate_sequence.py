f390d8b5ff2a9f956b847ae82efd0f9d6826d85a
Emma Davenport <emma.v.davenport@gmail.com>
2024-08-05 22:02:35 +0100

    generate sequence header file

--- scripts/generate_sequence.py
+++ scripts/generate_sequence.py
@@ -5,39 +5,58 @@ def main(args):
     with open(args.sequence_file, "r") as f:
         data = json.load(f)
 
-    sequence_length = len(data["steps"])
-
-    steps = ""
-    for step in data["steps"]:
-        type = step["type"]
-        channel = step["channel"]
-        note = step["note"]
-        velocity = step["velocity"]
-
-        glide = 0 if step["glide"] == False else 1
-
-        step_string = "{"
-        step_string += f"{type},{channel},{note},{velocity},{glide}"
-        step_string += "},\n"
-        steps += step_string
-
-    output_string = """#ifndef _SEQUENCE_H
+    with open(args.output, "w") as o:
+        o.write("""#ifndef _SEQUENCE_H
 #define _SEQUENCE_H
-#include \"midi.h\"
-"""
+#include "midi.h"
 
-    output_string += f"#define SEQUENCE_LENGTH {sequence_length}\n"
+""")
 
-    output_string += """MIDIPacket_TypeDef midi_sequence[] = {
-"""
-    output_string += steps
+        sequences = data["sequences"]
+        for sequence in sequences:
+            name = sequence["sequence_name"]
+            steps_name = sequence["steps_name"]
+            channel = sequence["channel"]
+            channel_string = f"CHANNEL_{channel}"
+            divisor = sequence["divisor"]
+            steps = sequence["steps"]
+            num_steps = len(steps)
+            sequence_string = """MIDISequence_TypeDef %s = {
+    %i,
+    0,
+    %i,
+    %s,
+    %s
+};
 
-    output_string += """};
-#endif
-"""
+""" % (name, divisor, num_steps, channel_string, steps_name)
 
-    with open(args.output, "w") as f:
-        f.write(output_string)
+            
+
+            step_string_array = []
+            for i, step in enumerate(steps):
+                end_of_sequence = 0
+                if i == len(steps) - 1:
+                    end_of_sequence = 1
+                note = step["note"]
+                type = step["type"]
+                glide = 0 if step["glide"] == False else 1
+                velocity = step["velocity"]
+                step_string = f"{{{type},{note},{velocity},{glide},{end_of_sequence}}}"
+                step_string_array.append(step_string)
+
+            steps_concat = ",\n\t".join(step_string_array)
+        
+            steps_string = """MIDIStep_TypeDef %s[] = {
+    %s
+};
+
+""" % (steps_name, steps_concat)
+
+            o.write(steps_string)
+            o.write(sequence_string)
+
+        o.write("#endif")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
