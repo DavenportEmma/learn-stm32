21224fb0ac3af0780113295fe18921b8688f5fa2
Emma Davenport <emma.v.davenport@gmail.com>
2024-08-05 20:44:46 +0100

    main: move tasks to separate file

--- examples/sequencer/src/main.c
+++ examples/sequencer/src/main.c
@@ -1,12 +1,17 @@
 #include "FreeRTOSConfig.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 #include "cmsis_gcc.h"
 #include "stm32f722xx.h"
 #include "uart.h"
 #include "midi.h"
 #include "start_up.h"
 #include "tasks.h"
+#include "sequence.h"
+#include "common.h"
+
+SemaphoreHandle_t MIDI_mutex;
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
     char msg[] = "stack overflow\n\r";
@@ -15,8 +20,12 @@ void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
 }
 
 int main(void) {
+    MIDI_mutex = xSemaphoreCreateMutex();
+
     start_up();
-    xTaskCreate(MIDI_task, "MIDI Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+    xTaskCreate(MIDI_task, "MIDI Task", configMINIMAL_STACK_SIZE, &sequence1, 1, NULL);
+    xTaskCreate(MIDI_task, "MIDI Task", configMINIMAL_STACK_SIZE, &sequence0, 1, NULL);
     vTaskStartScheduler();
 
     while(1) {
