45d593a3a1fc002ec3901d6dd5cb62498c990b3f
Emma Davenport <emma.v.davenport@gmail.com>
2024-08-05 15:54:46 +0100

    main: include tasks

--- examples/sequencer/src/main.c
+++ examples/sequencer/src/main.c
@@ -6,33 +6,7 @@
 #include "uart.h"
 #include "midi.h"
 #include "start_up.h"
-
-MIDIPacket_TypeDef midi_stack[] = {
-    {
-        NOTE_ON, CHANNEL_12, A2, 127
-    },
-    {
-        NOTE_ON, CHANNEL_12, C3, 126
-    },
-    {
-        NOTE_OFF, CHANNEL_12, A2, 0
-    },
-    {
-        NOTE_OFF, CHANNEL_12, C3, 0
-    },
-    {
-        NOTE_ON, CHANNEL_12, E3, 126
-    },
-    {
-        NOTE_ON, CHANNEL_12, G2, 126
-    },
-    {
-        NOTE_OFF, CHANNEL_12, E3, 0
-    },
-    {
-        NOTE_OFF, CHANNEL_12, G2, 0
-    },
-};
+#include "tasks.h"
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
     char msg[] = "stack overflow\n\r";
