299596b88821206b4f8e5a66bd3149e673f16150
Emma Davenport <emma.v.davenport@gmail.com>
2024-08-28 20:55:31 +0100

    step-input-sequencer: add example for step inputs
    this example lets you send serial packets to add notes to the sequence.
    the packets follow a custom protocol:
        start                         end
        f4 [status] [note] [velocity] f5

---
+++ examples/step-input-sequencer/src/tasks.c
@@ -0,0 +1,143 @@
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "tasks.h"
+#include "uart.h"
+#include "midi.h"
+#include "common.h"
+#include "timers.h"
+#include "config.h"
+
+static void flash_callback(TimerHandle_t xTimer) {
+    USER_LED_GPIO->ODR &= ~(1 << USER_LED_GPIO_PIN);
+}
+
+static void flash_tempo_led(xTimerHandle t) {
+    USER_LED_GPIO->ODR |= (1 << USER_LED_GPIO_PIN);
+    if(t) {
+        send_uart(STLINK_UART, "before\n\r", 8);
+        xTimerStart(t, 0);
+        send_uart(STLINK_UART, "after\n\r", 7);
+
+    } else {
+        char msg[] = "flash timer not initialised\n\r";
+        send_uart(STLINK_UART, msg, sizeof(msg)-1);
+    }
+}
+
+void uint8_t_to_char(char* str, uint8_t value) {
+    int i = 0;
+    uint8_t digit;
+    int start = i;
+    
+    // Handle zero as a special case
+    if (value == 0) {
+        str[i] = '\0';
+        str[i++] = '\r';
+        str[i++] = '\n';
+        str[i++] = '0';
+        return;
+    } else {
+        // Find the position to start inserting digits (we're inserting in reverse)
+        while (value > 0) {
+            digit = value % 10;         // Get the last digit
+            str[i++] = '0' + digit;     // Convert it to ASCII and store in the string
+            value /= 10;                // Remove the last digit
+        }
+        str[i++] = '\n';
+        str[i++] = '\r';
+        str[i] = '\0';                  // Null-terminate the string
+    }
+
+
+    // Reverse the string to correct the order of digits
+    for (int j = start, k = i - 1; j < k; j++, k--) {
+        char temp = str[j];
+        str[j] = str[k];
+        str[k] = temp;
+    }
+}
+
+void step_task(void *pvParameters) {
+    MIDISequence_TypeDef* mysequence = (MIDISequence_TypeDef*) pvParameters;
+
+    xTimerHandle t = xTimerCreate(
+        "flash timer",
+        pdMS_TO_TICKS(TEMPO_LED_FLASH_PERIOD),
+        pdFALSE,
+        NULL,
+        flash_callback
+    );
+
+    TickType_t lastWakeTime;
+    
+    while(1) {
+        lastWakeTime = xTaskGetTickCount();
+        
+        flash_tempo_led(t);
+
+        int counter = mysequence->counter;
+        if(xSemaphoreTake(sequence_mutex, portMAX_DELAY) == pdTRUE) {
+            if(rx_buffer_ready) {
+                rx_buffer_ready = 0;
+                rx_buffer_loc = 0;
+
+                mysequence->steps[counter].status = rx_buffer[1];
+                mysequence->steps[counter].note = rx_buffer[2];
+                mysequence->steps[counter].velocity = rx_buffer[3];
+            }
+            xSemaphoreGive(sequence_mutex);
+        }
+
+        MIDIStep_TypeDef previous_step;
+        MIDIStep_TypeDef this_step = mysequence->steps[counter];
+
+        if(counter == 0) {
+            previous_step = mysequence->steps[mysequence->num_steps - 1];
+        } else {
+            previous_step = mysequence->steps[counter - 1];
+        }
+
+        MIDIPacket_TypeDef previous_packet = {
+            previous_step.status,
+            mysequence->channel,
+            previous_step.note,
+            previous_step.velocity,
+        };
+
+        MIDIPacket_TypeDef this_packet = {
+            this_step.status,
+            mysequence->channel,
+            this_step.note,
+            this_step.velocity,
+        };
+        
+        if(xSemaphoreTake(MIDI_mutex, portMAX_DELAY) == pdTRUE) {
+            if(previous_step.glide == 0) {
+                previous_packet.status = NOTE_OFF;
+                send_midi_note(MIDI_UART, &previous_packet);
+            }
+
+            send_midi_note(MIDI_UART, &this_packet);
+            
+            
+            if(previous_step.glide == 1) {
+                previous_packet.status = NOTE_OFF;
+                send_midi_note(MIDI_UART, &previous_packet);
+            }
+
+            xSemaphoreGive(MIDI_mutex);
+        }
+
+        if(xSemaphoreTake(sequence_mutex, portMAX_DELAY) == pdTRUE) {
+            mysequence->counter++;
+            if(mysequence->counter >= mysequence->num_steps) {
+                mysequence->counter = 0;
+            }
+            xSemaphoreGive(sequence_mutex);
+        }
+
+        vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(STEP_MS));
+    }
+}
