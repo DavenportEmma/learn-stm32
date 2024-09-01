d079bc09190f884ca11a93cb682278e95b147619
Emma Davenport <emma.v.davenport@gmail.com>
2024-09-01 22:04:26 +0100

    menu: add enum for menu page IDs

---
+++ include/menu.h
@@ -0,0 +1,14 @@
+#ifndef _MENU_H
+#define _MENU_H
+
+typedef enum {
+    SEQUENCE_SELECT = 0,
+    FUNCTION_SELECT = 1,
+    STEP_SELECT = 2,
+    MODIFY_STEP = 3,
+    PITCH_MENU = 4,
+    VELOCITY_MENU = 5,
+    GLIDE_MENU = 6,
+} MenuLUT;
+
+#endif
